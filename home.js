a85252692a953fc7c1d59551fe98794230a862d1
pavinane <pavimegan77@gmail.com>
2020-06-13 21:20:14 +0530

    variable learn

--- home.js
+++ home.js
@@ -1,4 +1,19 @@
 console.log('hello');
 console.log('hello pavi');
-console.log('hello vaishu');
-alert('hello this pavi');
+
+//   this is command line ,it may be not uses (or) explain the code
+// alert('hello this pavi');
+
+// Variables
+ var b ='practice';
+ console.log(b);
+
+ var SomeNumber ="45";
+ console.log(SomeNumber);
+
+//  document.getElementById("someText").innerHTML="Hey There";
+
+ var age = prompt('Hey What is your age ?') //it is like a question
+
+ document.getElementById("someText").innerHTML=age;
+ console.log(age)
