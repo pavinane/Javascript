e852b8ac06503558bb81c09ea7cfc37292748a0e
pavinane <pavimegan77@gmail.com>
2020-06-13 21:51:01 +0530

    basic number in javascript

--- home.js
+++ home.js
@@ -13,7 +13,31 @@ console.log('hello pavi');
 
 //  document.getElementById("someText").innerHTML="Hey There";
 
- var age = prompt('Hey What is your age ?') //it is like a question
+//  var age = prompt('Hey What is your age ?') //it is like a question
 
- document.getElementById("someText").innerHTML=age;
- console.log(age)
+//  document.getElementById("someText").innerHTML=age;
+
+
+//  Numbers in Javascript
+
+ var num1 = 10;
+//  num1 = num1 +2;
+
+// increment num1 by 1
+ num1++; 
+ console.log(num1);
+
+ var num2 = 12;
+ //Decrement num2 by 1
+ num2--;
+ console.log(num2);
+
+ //Divide, Multiple * ,reminder%
+ var num3 = 8;
+  console.log(num3 % 6); // it's like also suitable above heading
+
+  // Increment ,Decrement by any Number
+  var num4 =15;
+  num4 += 15;
+    console.log(num4);
+  
