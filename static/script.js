7b7f15d3dbf2857aaa833e3e4b1d80734aed1271
pavinane <pavimegan77@gmail.com>
2020-06-25 10:07:00 +0530

    generate cat

--- static/script.js
+++ static/script.js
@@ -4,7 +4,7 @@ function ageinDays() {
     var birthYear = prompt("what year were you born... Good friend?");
     var age = (2020-birthYear) * 365;
     var h3 = document.createElement('h3');
-    var textAnswer = document.createTextNode ('Yor are' + age + 'days old ')
+    var textAnswer = document.createTextNode (' Yor are ' + age + ' days old ')
   h3.setAttribute ('id','ageinDays');
   h3.appendChild(textAnswer);
   document.getElementById('flex-box-result').appendChild(h3);
@@ -13,4 +13,14 @@ function ageinDays() {
 
 function reset () {
     document.getElementById('ageinDays').remove();
+}
+
+
+// Challenge2 : Generate Cat
+
+function generateCat () {
+  var image = document.createElement('img');
+  var div = document.getElementById('flex-cat-gen');
+  image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+  div.appendChild(image)
 }
